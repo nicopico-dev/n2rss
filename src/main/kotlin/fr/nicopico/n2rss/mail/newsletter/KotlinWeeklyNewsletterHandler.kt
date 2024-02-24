acb85511db0ed557e8ed6d4342060ebd19a28af9
Nicolas PICON <nicopico.dev@gmail.com>
2024-02-24 19:06:26 +0100

    chore: create KotlinWeeklyNewsletterHandler

---
+++ src/main/kotlin/fr/nicopico/n2rss/mail/newsletter/KotlinWeeklyNewsletterHandler.kt
@@ -0,0 +1,29 @@
+package fr.nicopico.n2rss.mail.newsletter
+
+import fr.nicopico.n2rss.models.Article
+import fr.nicopico.n2rss.models.Email
+import fr.nicopico.n2rss.models.Newsletter
+import org.jsoup.Jsoup
+import org.jsoup.safety.Safelist
+
+class KotlinWeeklyNewsletterHandler : NewsletterHandler {
+
+    override val newsletter: Newsletter = Newsletter(
+        name = "Kotlin Weekly",
+        websiteUrl = "http://kotlinweekly.net/",
+    )
+
+    override fun canHandle(email: Email): Boolean {
+        return email.sender.email.contains("mailinglist@kotlinweekly.net")
+    }
+
+    override fun extractArticles(email: Email): List<Article> {
+        val cleanedHtml = Jsoup.clean(
+            email.content,
+            Safelist.basic(),
+        )
+        val document = Jsoup.parseBodyFragment(cleanedHtml)
+        // TODO Parse document
+        return emptyList()
+    }
+}
