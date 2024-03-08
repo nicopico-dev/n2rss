e1c1622ec9918470809ab09bc62e13dff6af6af1
Nicolas PICON <nicopico.dev@gmail.com>
2024-03-08 23:55:55 +0100

    refactor: use constructor injection for NewsletterValueConverter

--- src/main/kotlin/fr/nicopico/n2rss/data/NewsletterValueConverter.kt
+++ src/main/kotlin/fr/nicopico/n2rss/data/NewsletterValueConverter.kt
@@ -2,27 +2,14 @@ package fr.nicopico.n2rss.data
 
 import fr.nicopico.n2rss.mail.newsletter.NewsletterHandler
 import fr.nicopico.n2rss.models.Newsletter
-import org.springframework.context.ApplicationContext
-import org.springframework.context.ApplicationContextAware
 import org.springframework.data.convert.PropertyValueConverter
 import org.springframework.data.convert.ValueConversionContext
 import org.springframework.stereotype.Component
 
 @Component
-class NewsletterValueConverter
-    : PropertyValueConverter<Newsletter, String, ValueConversionContext<*>>, ApplicationContextAware {
-
-    private lateinit var applicationContext: ApplicationContext
-
-    @Suppress("UNCHECKED_CAST")
+class NewsletterValueConverter(
     private val handlers: List<NewsletterHandler>
-        get() = applicationContext
-            .getBean("newsletterHandlers", List::class.java)
-                as List<NewsletterHandler>
-
-    override fun setApplicationContext(applicationContext: ApplicationContext) {
-        this.applicationContext = applicationContext
-    }
+) : PropertyValueConverter<Newsletter, String, ValueConversionContext<*>> {
 
     override fun read(value: String, context: ValueConversionContext<*>): Newsletter? {
         return handlers
