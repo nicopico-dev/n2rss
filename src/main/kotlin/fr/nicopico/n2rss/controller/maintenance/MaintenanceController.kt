06a60d73aa4eeebb6e18ed07d010b65d72ccf887
Nicolas Picon <nicopico.dev@gmail.com>
2024-04-13 16:12:19 +0200

    chore: copyrights

--- src/main/kotlin/fr/nicopico/n2rss/controller/maintenance/MaintenanceController.kt
+++ src/main/kotlin/fr/nicopico/n2rss/controller/maintenance/MaintenanceController.kt
@@ -15,7 +15,6 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  */
-
 package fr.nicopico.n2rss.controller.maintenance
 
 import fr.nicopico.n2rss.config.N2RssProperties
--- src/main/kotlin/fr/nicopico/n2rss/models/NewsletterInfo.kt
+++ src/main/kotlin/fr/nicopico/n2rss/models/NewsletterInfo.kt
@@ -15,7 +15,6 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  */
-
 package fr.nicopico.n2rss.models
 
 import kotlinx.datetime.LocalDate
