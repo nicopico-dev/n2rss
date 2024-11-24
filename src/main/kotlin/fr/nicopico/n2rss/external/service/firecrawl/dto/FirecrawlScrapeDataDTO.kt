70586cce98bbfae04ea437ff7248f074d8b2737a
Nicolas Picon <nicopico.dev@gmail.com>
2024-11-24 12:27:33 +0100

    feat: integrate Firecrawl API

---
+++ src/main/kotlin/fr/nicopico/n2rss/external/service/firecrawl/dto/FirecrawlScrapeDataDTO.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2024 Nicolas PICON
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package fr.nicopico.n2rss.external.service.firecrawl.dto
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class FirecrawlScrapeDataDTO(
+    @JsonProperty("markdown")
+    val markdown: String,
+
+    @JsonProperty("html")
+    val html: String? = null,
+
+    @JsonProperty("rawHtml")
+    val rawHtml: String? = null,
+
+    @JsonProperty("screenshot")
+    val screenshot: String? = null,
+
+    @JsonProperty("links")
+    val links: List<String> = emptyList(),
+
+    @JsonProperty("actions")
+    val actions: Map<String, Any>? = null,
+
+    @JsonProperty("metadata")
+    val metadata: Map<String, Any>,
+
+    @JsonProperty("llm_extraction")
+    val llmExtraction: Map<String, Any>? = null,
+
+    @JsonProperty("warning")
+    val warning: String? = null,
+)
