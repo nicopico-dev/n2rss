c414be7540dae09b53c1db9fbec47457c9a751de
Nicolas Picon <nicopico.dev@gmail.com>
2025-03-23 22:42:26 +0100

    feat: parse GDIY emails

--- src/main/kotlin/fr/nicopico/n2rss/newsletter/handlers/GDIYNewsletterHandler.kt
+++ src/main/kotlin/fr/nicopico/n2rss/newsletter/handlers/GDIYNewsletterHandler.kt
@@ -15,12 +15,18 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  */
-
 package fr.nicopico.n2rss.newsletter.handlers
 
 import fr.nicopico.n2rss.mail.models.Email
+import fr.nicopico.n2rss.mail.models.html
+import fr.nicopico.n2rss.newsletter.handlers.exception.NewsletterParsingException
 import fr.nicopico.n2rss.newsletter.models.Article
 import fr.nicopico.n2rss.newsletter.models.Newsletter
+import fr.nicopico.n2rss.utils.url.toUrlOrNull
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
+import org.jsoup.safety.Safelist
+import java.util.Locale
 
 class GDIYNewsletterHandler : NewsletterHandlerSingleFeed {
 
@@ -35,6 +41,57 @@ class GDIYNewsletterHandler : NewsletterHandlerSingleFeed {
     }
 
     override fun extractArticles(email: Email): List<Article> {
-        return emptyList()
+        val cleanedHtml = Jsoup.clean(
+            email.content.html,
+            Safelist.none()
+                .addTags("a", "span", "p")
+                .addAttributes("a", "href")
+                .addAttributes("span", "style")
+        )
+        // TODO println(Jsoup.parseBodyFragment(email.content.html))
+        val document = Jsoup.parseBodyFragment(cleanedHtml)
+        // TODO println(document)
+
+        return document
+            .select("a[href] > span[style*=color]")
+            .filter { element ->
+                element.matchColor("#257953")
+                    && element.text().matches(ARTICLE_TITLE_REGEX)
+            }
+            .map { linkSpan ->
+                val link = requireNotNull(linkSpan.parent())
+                val description = link.nextElementSiblings()
+                    .takeWhile { element -> element.tagName() == "p" }
+                    .joinToString("\n\n") { it.text() }
+                    .trim()
+
+                Article(
+                    title = link.text(),
+                    link = link.attr("href").toUrlOrNull()
+                        ?: throw NewsletterParsingException("No valid link for article"),
+                    description = description,
+                )
+            }
+    }
+
+    private fun Element.matchColor(hexColor: String): Boolean {
+        val extractedColor = extractCssColor() ?: return false
+        return extractedColor.equals(hexColor, ignoreCase = true)
+    }
+
+    private fun Element.extractCssColor(): String? {
+        val style = attr("style")
+        val hexColorRegex = Regex("color\\s*:\\s*(#[A-Fa-f0-9]{6}|#[A-Fa-f0-9]{3})\\b")
+        val rgbColorRegex = Regex("color\\s*:\\s*rgb\\((\\d+),\\s*(\\d+),\\s*(\\d+)\\)")
+
+        return hexColorRegex.find(style)?.groupValues?.get(1)
+            ?: rgbColorRegex.find(style)?.let {
+                val (r, g, b) = it.destructured
+                String.format(Locale.ROOT, "#%02X%02X%02X", r.toInt(), g.toInt(), b.toInt())
+            }
+    }
+
+    companion object {
+        private val ARTICLE_TITLE_REGEX = Regex("#\\d+ - .*")
     }
 }
