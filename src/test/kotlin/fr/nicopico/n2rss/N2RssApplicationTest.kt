6505c46e86ca2de34f3b041f037a2a00dfb76410
Nicolas Picon <nicopico.dev@gmail.com>
2025-03-29 15:58:27 +0100

    chore: check the app production configuration

--- src/test/kotlin/fr/nicopico/n2rss/N2RssApplicationTest.kt
+++ src/test/kotlin/fr/nicopico/n2rss/N2RssApplicationTest.kt
@@ -19,6 +19,8 @@ package fr.nicopico.n2rss
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
 
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.MOCK,
@@ -27,10 +29,61 @@ import org.springframework.boot.test.context.SpringBootTest
         "spring.profiles.active=local, test",
     ],
 )
-class N2RssApplicationTest {
+class N2RssApplicationDevTest {
 
     @Test
-    fun `application should start with default configuration`() {
+    fun `application should start with dev configuration`() {
         // The test is just loading the Spring Boot context
+    }
+}
+
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+    useMainMethod = SpringBootTest.UseMainMethod.WHEN_AVAILABLE,
+    properties = [
+        "spring.profiles.active=local, test",
+        "spring.config.location=file:./deploy/application.properties"
+    ],
+)
+class N2RssApplicationProdTest {
+
+    @Test
+    fun `application should start with production configuration`() {
+        // The test is just loading the Spring Boot context
+    }
+
+    companion object {
+        @JvmStatic
+        @DynamicPropertySource
+        fun environmentVariables(
+            registry: DynamicPropertyRegistry
+        ) {
+            registry.add("N2RSS_ANALYTICS_HOSTNAME") { "n2rss.nicopico.fr" }
+            registry.add("N2RSS_ANALYTICS_PROFILE") { "simple-analytics" }
+            registry.add("N2RSS_ANALYTICS_UA") { "<user-agent>" }
+
+            registry.add("N2RSS_BASE_URL") { "https://n2rss.nicopico.fr" }
+
+            registry.add("N2RSS_DATABASE_PASSWORD") { "" }
+            registry.add("N2RSS_DATABASE_SCHEMA") { "PUBLIC" }
+            registry.add("N2RSS_DATABASE_URL") { "jdbc:h2:mem:test-db" }
+            registry.add("N2RSS_DATABASE_USERNAME") { "SA" }
+
+            registry.add("N2RSS_DISABLED_NEWSLETTERS") { "" }
+
+            registry.add("N2RSS_EMAIL_HOST") { "<email-host>" }
+            registry.add("N2RSS_EMAIL_INBOX_FOLDERS") { "INBOX,Spam" }
+            registry.add("N2RSS_EMAIL_PASSWORD") { "<email-password>" }
+            registry.add("N2RSS_EMAIL_USERNAME") { "<email-username>" }
+
+            registry.add("N2RSS_GITHUB_ACCESS_TOKEN") { "<github-access-token>" }
+
+            registry.add("N2RSS_PERSISTENCE_MODE") { "DEFAULT" }
+
+            registry.add("N2RSS_RECAPTCHA_SECRET_KEY") { "<recaptcha-secret-key>" }
+            registry.add("N2RSS_RECAPTCHA_SITE_KEY") { "<recaptcha-site-key>" }
+
+            registry.add("N2RSS_SECRET_KEY") { "<secret>" }
+        }
     }
 }
