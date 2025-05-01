78cedee17448cf6e0c7c48edb4f256c5abbbaf23
Nicolas Picon <nicopico.dev@gmail.com>
2025-05-01 16:13:39 +0200

    chore: add integration test for beehiiv URLs

--- src/test/kotlin/fr/nicopico/n2rss/utils/url/RestClientExtKtTest.kt
+++ src/test/kotlin/fr/nicopico/n2rss/utils/url/RestClientExtKtTest.kt
@@ -23,6 +23,7 @@ import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import java.net.URI
 
@@ -122,5 +123,20 @@ class RestClientExtKtTest {
 
         // THEN
         result shouldBe mapOf(originalUri to null)
+    }
+
+    @Disabled
+    @Test
+    fun `should resolve 'beehiiv' urls`() {
+        val userAgent = "n2rss"
+        val beehiivUrl =
+            URI("https://link.mail.beehiiv.com/ss/c/u001.3a5P_SwQzY5x8USD2q4p0r7tZ4Xc_IMfzOhNH-sZPqXF5edqv_aYXhBXdCzcRVykDyq9Wl9a1Ge1hLkoeCwntpQkvfL-5h3Xz0oMO0MNUb4JOZkpL15kAwrX55aT-RUjLDbPVBr78sxN0T17LYZS4Ar3QuxRWfDySw2dWCyJIvNwmllgb9FqevhlQtIWQwJomy11i66WPmIY76Tj39FzlimpG3Ylm2Ay4QNS_wmQzYhLJrM85OjSu_-5vq3RR7SolPqm7MhUq_R5Y3pn_KLdo_cS7p1n9gckSSXS1rLk9LH9PWAEdKGOPayosqQ2YCNR/4bo/zbVYybWiRwSiQr1bey4J-w/h10/h001.2nd4OWD1oRoMgzUpVtSORAo4DI3VH1mAH-36d0sUrRg")
+        val expectedUrl = URI("https://www.qawolf.com/webinars/ai-prompt-evaluations-beyond-golden-datasets")
+
+        val result = runBlocking {
+            resolveUris(userAgent, listOf(beehiivUrl))
+        }
+
+        result[beehiivUrl] shouldBe expectedUrl
     }
 }
