ccd6fc6bd3f7dea8352ab5cfb88fab0f3df5bfd9
Nicolas Picon <nicopico.dev@gmail.com>
2025-05-18 23:35:06 +0200

    feat: add "Blog du Modérateur" newsletter support (#233)

---
+++ src/main/kotlin/fr/nicopico/n2rss/newsletter/handlers/BlogDuModerateurNewsletterHandler.kt
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) 2025 Nicolas PICON
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+package fr.nicopico.n2rss.newsletter.handlers
+
+import fr.nicopico.n2rss.mail.models.Email
+import fr.nicopico.n2rss.mail.models.html
+import fr.nicopico.n2rss.newsletter.handlers.exception.NewsletterParsingException
+import fr.nicopico.n2rss.newsletter.models.Article
+import fr.nicopico.n2rss.newsletter.models.Newsletter
+import fr.nicopico.n2rss.utils.url.toUrlOrNull
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.safety.Safelist
+import org.springframework.stereotype.Component
+
+@Component
+class BlogDuModerateurNewsletterHandler : NewsletterHandlerMultipleFeeds {
+
+    override val newsletters: List<Newsletter> = listOf(
+        mainNewsletter,
+        toolsNewsletter,
+    )
+
+    override fun canHandle(email: Email): Boolean {
+        return email.sender.email == "newsletter@blogdumoderateur.com"
+    }
+
+    override fun extractArticles(email: Email): Map<Newsletter, List<Article>> {
+        val cleanedHtml = Jsoup.clean(
+            email.content.html,
+            Safelist.basic()
+                .addAttributes("a", "class"),
+        )
+        val document = Jsoup.parseBodyFragment(cleanedHtml)
+        val documentContent = document.body().toString()
+        val bestToolsIndex = documentContent.indexOf("Découvrez les meilleurs outils web")
+
+        val articles: List<Article>
+        val tools: List<Article>
+        if (bestToolsIndex == -1) {
+            articles = document.extractArticles()
+            tools = emptyList()
+        } else {
+            val articlesDocument = documentContent.substring(0, bestToolsIndex)
+                .let { Jsoup.parseBodyFragment(it) }
+            val toolsDocument = documentContent.substring(bestToolsIndex)
+                .let { Jsoup.parseBodyFragment(it) }
+            articles = articlesDocument.extractArticles()
+            tools = toolsDocument.extractArticles()
+        }
+
+        return mapOf(
+            mainNewsletter to articles,
+            toolsNewsletter to tools,
+        )
+    }
+
+    private fun Document.extractArticles(): List<Article> {
+        return select("a.title")
+            .map { linkElement ->
+                val title = linkElement.text()
+                val link = linkElement.attr("href").toUrlOrNull()
+                    ?: throw NewsletterParsingException("Could not retrieve the link for article '$title'")
+                // Some articles have a description as a secondary <a> element
+                val description = linkElement.nextElementSibling()
+                    ?.takeIf {
+                        val elementClass = it.attr("class")
+                        it.tagName() == "a"
+                            && "title" !in elementClass
+                            && "dark-style-color3" in elementClass
+                    }
+                    ?.text()
+
+                Article(
+                    title = title,
+                    link = link,
+                    description = description ?: "",
+                )
+            }
+    }
+
+    companion object {
+        const val BDM_MAIN_NEWSLETTER_CODE = "bdm"
+        const val BDM_TOOLS_NEWSLETTER_CODE = "bdm-tools"
+
+        private val mainNewsletter = Newsletter(
+            code = BDM_MAIN_NEWSLETTER_CODE,
+            name = "Blog du Modérateur",
+            websiteUrl = "https://www.blogdumoderateur.com",
+            notes = "Articles",
+            feedTitle = "Blog du Modérateur (Articles)",
+        )
+
+        private val toolsNewsletter = Newsletter(
+            code = BDM_TOOLS_NEWSLETTER_CODE,
+            name = "Blog du Modérateur",
+            websiteUrl = "https://www.blogdumoderateur.com",
+            notes = "Outils",
+            feedTitle = "Blog du Modérateur (Outils)"
+        )
+    }
+}
---
+++ src/test/kotlin/fr/nicopico/n2rss/newsletter/handlers/BlogDuModerateurNewsletterHandlerTest.kt
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2025 Nicolas PICON
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+package fr.nicopico.n2rss.newsletter.handlers
+
+import fr.nicopico.n2rss.mail.models.Email
+import io.kotest.assertions.assertSoftly
+import io.kotest.assertions.withClue
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.kotlinx.datetime.shouldHaveSameDayAs
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class BlogDuModerateurNewsletterHandlerTest : BaseNewsletterHandlerTest<BlogDuModerateurNewsletterHandler>(
+    handlerProvider = ::BlogDuModerateurNewsletterHandler,
+    stubsFolder = "Blog du Modérateur"
+) {
+    @Nested
+    inner class EmailProcessingTest {
+        @Test
+        fun `should extract articles from email about ChatGPT and Meta AI`() {
+            // GIVEN
+            val email: Email =
+                loadEmail("stubs/emails/Blog du Modérateur/Générateur d'images de ChatGPT, retour aux sources pour Facebook, Meta AI sur WhatsApp, TikTok Shop.eml")
+
+            // WHEN
+            val publications = handler.process(email)
+
+            // THEN
+            publications shouldHaveSize 2
+
+            // Articles publication
+            val articlesPublication = publications[0]
+            assertSoftly(articlesPublication) {
+                title shouldBe "Générateur d'images de ChatGPT, retour aux sources pour Facebook, Meta AI sur WhatsApp, TikTok Shop"
+                date shouldHaveSameDayAs (email.date)
+                newsletter.code shouldBe "bdm"
+            }
+
+            withClue("article titles") {
+                articlesPublication.articles.map { it.title } shouldBe listOf(
+                    "ChatGPT abandonne DALL-E et se dote enfin d’un bon générateur d’images",
+                    "Facebook lance un fil uniquement pour les amis : un retour aux sources ?",
+                    "Comment choisir son hébergement web : les conseils de o2switch",
+                    "La Sphère de Las Vegas, une aberrante prouesse technologique ?",
+                    "Réseaux sociaux : les étapes pour devenir une marque mature",
+                    "Meta AI sur WhatsApp : un assistant IA encombrant et décevant",
+                    "IA, tendances TikTok, insights consommateurs : quelles innovations social media en 2025 ?",
+                    "Mode vocal avancé sur ChatGPT : 5 cas d’usages",
+                    "Pourquoi le social listening doit être multi-canal",
+                    "Étude : 45 % des cadres ne maîtrisent pas les outils numériques en 2025",
+                    "Les tendances réseaux sociaux 2025 pour une stratégie efficace",
+                    "TikTok Shop arrive en France : les marques doivent-elles miser sur le social commerce ?",
+                    "5 formations pour apprendre le marketing d’influence",
+                    "IA, régulation et protection des données : la vision de Zendesk",
+                    "Test automatisé et test manuel : quelles différences ?",
+                    "« X va tanguer mais sera difficile à déloger » : We Are Social refait le match entre X, Threads et Bluesky",
+                    "11 événements web à ne pas manquer en avril 2025",
+                    "Google lance Gemini 2.5 pour concurrencer la série de modèles « o » d’OpenAI",
+                    "5 applications mobiles autrefois incontournables mais tombées dans l’oubli",
+                    "SEO : comment la presse s’empare de l’IA pour améliorer son référencement",
+                    "Webinar : comment maîtriser le marketing digital dans la beauté et les cosmétiques ?",
+                )
+            }
+
+            withClue("article descriptions") {
+                articlesPublication.articles.map { it.description } shouldBe listOf(
+                    "OpenAI lance 4o Image Generation, un nouveau modèle de génération d'images. Celui-ci remplace désormais DALL-E dans ChatGPT. Voici ce qu'il vaut !",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "Rendez-vous le 8 avril pour un webinar dédié à cette industrie en pleine croissance et qui offre de belles opportunités de carrière.",
+                )
+            }
+
+            // Tools Publication
+            val toolsPublication = publications[1]
+            assertSoftly(toolsPublication) {
+                title shouldBe "Générateur d'images de ChatGPT, retour aux sources pour Facebook, Meta AI sur WhatsApp, TikTok Shop"
+                date shouldHaveSameDayAs (email.date)
+                newsletter.code shouldBe "bdm-tools"
+            }
+
+            withClue("tool titles") {
+                toolsPublication.articles.map { it.title } shouldBe listOf(
+                    "Evoliz : un outil pour gérer ses factures, devis et bons de commande",
+                    "Studio Creatio : un outil pour créer des applis et automatiser les workflows",
+                    "Sage 50 : un logiciel comptable avec des options de gestion d'entreprise",
+                    "Gouti : une plateforme de planification des ressources d'entreprise",
+                    "Metricool : une solution pour gérer vos réseaux sociaux et analyser vos performances",
+                    "Gestion de projet : 72 outils pour gérer et planifier ses tâches",
+                )
+            }
+        }
+    }
+}
