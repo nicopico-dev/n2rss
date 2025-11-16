0df3ff46c04236c2541c7f2f6c9af92ac2939190
Nicolas Picon <nicopico.dev@gmail.com>
2025-11-16 21:51:51 +0100

    feat: open RSS feed from home page

--- server/src/main/kotlin/fr/nicopico/n2rss/controller/rss/RssViewerController.kt
+++ server/src/main/kotlin/fr/nicopico/n2rss/controller/rss/RssViewerController.kt
@@ -15,20 +15,42 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  */
-
 package fr.nicopico.n2rss.controller.rss
 
+import fr.nicopico.n2rss.config.N2RssProperties
+import jakarta.servlet.http.HttpServletRequest
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestParam
+
+private val LOG = LoggerFactory.getLogger(RssViewerController::class.java)
 
 @Controller
-class RssViewerController {
+class RssViewerController(
+    private val feedProperties: N2RssProperties.FeedsProperties,
+) {
 
     @GetMapping("rss/viewer")
-    fun feedViewer(): String {
-        // TODO Pass feed through model
-        // TODO Add the raw RSS url in the page
-        // TODO Handle analytics
+    fun feedViewer(
+        request: HttpServletRequest,
+        model: Model,
+        @RequestParam(value = "code") code: String,
+        @Suppress("unused") /* Used by AnalyticsAspect */
+        @RequestHeader(value = "User-Agent") userAgent: String,
+    ): String {
+        val hostname = request.getHeader("Host") ?: request.serverName
+        val scheme = if (feedProperties.forceHttps) "https" else request.scheme
+        val feedUrl = "$scheme://$hostname/rss/$code"
+
+        LOG.debug("Opening RSS viewer for url: {}", feedUrl)
+
+        with(model) {
+            addAttribute("feedUrl", feedUrl)
+        }
+
         return "rss-viewer"
     }
 }
